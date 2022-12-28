278480f5c3f44bdeba7bc1b3a85b4e7350c2a118
Wojciech <wojciechstatek@gmail.com>
2022-12-28 19:57:19 +0100

    add cypress

---
+++ cypress/e2e/goit-task.cy.js
@@ -0,0 +1,15 @@
+describe('', () => {
+  const loginUrl = 'https://www.edu.goit.global/pl/account/login'
+  const email = 'user888@gmail.com'
+  const password = '1234567890'
+
+  it('oddaj pracę domową', () => {
+    cy.visit(loginUrl)
+    cy.get('#user_email').type(email)
+    cy.get('#user_password').type(password)
+    cy.get('button[type="submit"]').click()
+    cy.get('button').contains('Przejdź do kursu').click()
+    cy.get('span').contains('Zadanie domowe').click()
+    cy.get('button').contains('Ponownie prześlij pracę domową').click()
+  })
+})
