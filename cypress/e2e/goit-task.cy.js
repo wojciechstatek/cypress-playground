d36a2c1874177f7236e28813cfe64df83bc34237
Wojciech <wojciechstatek@gmail.com>
2022-12-29 09:37:04 +0100

    description added

--- cypress/e2e/goit-task.cy.js
+++ cypress/e2e/goit-task.cy.js
@@ -1,9 +1,9 @@
-describe('', () => {
+describe('Trener QA Automation', () => {
   const loginUrl = 'https://www.edu.goit.global/pl/account/login'
   const email = 'user888@gmail.com'
   const password = '1234567890'
 
-  it('oddaj pracę domową', () => {
+  it('Zadanie testowe', () => {
     cy.visit(loginUrl)
     cy.get('#user_email').type(email)
     cy.get('#user_password').type(password)
